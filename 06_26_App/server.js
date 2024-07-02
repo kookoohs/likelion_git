88ff594f5d1fac7feec37860d895c3a5891a21e1
kookoohs <150266687+kookoohs@users.noreply.github.com>
2024-07-02 20:27:47 +0900

    Update server.js

--- 06_26_App/server.js
+++ 06_26_App/server.js
@@ -1,30 +1,33 @@
-const setup=require('./db_setup');
-const express=require('express');
+const dotenv = require('dotenv').config();  // 최상단에 위치
+const setup = require('./db_setup');
+const express = require('express');
 
-const app=express();
+const app = express();
 
-const session=require('express-session');
+const session = require('express-session');
 app.use(session({
-    secret:"암호화키",
-    resave:false,
-    saveUninitialized:false,
+    secret: "암호화키",
+    resave: false,
+    saveUninitialized: false,
 }));
 
-const cookieParser=require('cookie-parser');
+const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
-const bodyParser= require('body-parser');
-app.use(bodyParser.urlencoded({extended:true}));
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.render('index.ejs');
 });
 
-app.use('/',require('./routes/account'));
-app.use('/',require('./routes/post'));
+app.use('/', require('./routes/account'));
+app.use('/', require('./routes/post'));
 
-app.listen(process.env.WEB_PORT, async()=>{
-    //DB 구축 후 server 준비
+app.listen(process.env.WEB_PORT, async () => {
+    // 환경 변수 출력
+    console.log('WEB_PORT:', process.env.WEB_PORT);
+    // DB 구축 후 server 준비
     await setup();    // 비동기적으로 작동
-    console.log('8080 서버 준비...')
+    console.log(`${process.env.WEB_PORT} 서버 준비...`);
 });
