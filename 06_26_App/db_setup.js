50654eee3547af7f577ca34f354fddd0e210c673
kookoohs <150266687+kookoohs@users.noreply.github.com>
2024-07-02 20:25:48 +0900

    Update db_setup.js

--- 06_26_App/db_setup.js
+++ 06_26_App/db_setup.js
@@ -1,43 +1,47 @@
-const dotenv=require('dotenv').config();
-const{MongoClient}=require('mongodb');
-const mysql=require('mysql');
+const dotenv = require('dotenv').config();  // 최상단에 위치
+const { MongoClient } = require('mongodb');
+const mysql = require('mysql');
 
 let mongodb;
 let mysqldb;
 
-const setup=async()=>{ 
-    if(mongodb&&mysqldb){
+const setup = async () => { 
+    if (mongodb && mysqldb) {
         console.log('DB already setup');
-        return {mongodb,mysqldb};
+        return { mongodb, mysqldb };
     }
 
-    try{
-        const mongoUrl =process.env.MONGO_URL;
-        //비동기적으로 동작! => 기다렸다가 동작해야됨
-        const mongoConn=await MongoClient.connect(mongoUrl,{
-            useNewUrlParser:true,
-            useUnifiedTopology:true,
+    try {
+        // 환경 변수 출력
+        console.log('MONGO_URL:', process.env.MONGO_DB_URL);
+        console.log('MONGO_DB:', process.env.MONGO_DB);
+        console.log('MYSQL_HOST:', process.env.MYSQL_HOST);
+        console.log('MYSQL_USER:', process.env.MYSQL_USER);
+        console.log('MYSQL_PW:', process.env.MYSQL_PW);
+        console.log('MYSQL_DB:', process.env.MYSQL_DB);
+
+        const mongoUrl = process.env.MONGO_DB_URL;
+        const mongoConn = await MongoClient.connect(mongoUrl, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
         });
-        mongodb=mongoConn.db(process.env.MONGO_DB);
+        mongodb = mongoConn.db(process.env.MONGO_DB);
         console.log('몽고 DB 접속 성공...');
 
-        // 비동기 함수 아님
-        mysqldb=mysql.createConnection({
+        mysqldb = mysql.createConnection({
             host: process.env.MYSQL_HOST,
-            user:process.env.MYSQL_USER,
-            password:process.env.MYSQL_PW,
-            database:process.env.MYSQL_DB,
+            user: process.env.MYSQL_USER,
+            password: process.env.MYSQL_PW,
+            database: process.env.MYSQL_DB,
         });
-        mysqldb.connect();
+        mysqldb.connect();  
         console.log('MySQL 접속 성공...');
 
-        return {mongodb,mysqldb};
-    } catch(err){
-        console.error("DB 접속 실패",err);
+        return { mongodb, mysqldb };
+    } catch (err) {
+        console.error("DB 접속 실패", err);
         throw err;
     }
 };
 
-
-
-module.exports=setup;
+module.exports = setup;
